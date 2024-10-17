b13867062ec422e8591dadb986fee0e3ed73b992
alarie-c <colin@colinheathcott.com>
2024-10-17 00:04:29 -0400

    Get file path + source as string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +6,10 @@ fn main() {
 
     if args.len() == 2 && dbga {
         // File path is specified
+        let path = &args[1];
+        let src = fs::read_to_string(&path).expect("Error reading source file");
+
+        // TODO: Lexer
     } else if args.len() < 2 && dbga {
         eprintln!("Please specify a file path");
         std::process::exit(1);
